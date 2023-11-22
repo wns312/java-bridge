6b1997a167f089b1c4ca17d37435902e5b44970a
wns312 <wns312@gmail.com>
2023-11-22 16:58:43 +0900

    feat: 이동 후 다리 출력 기능 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.controller.BridgeGameController;
 import bridge.domain.BridgeMaker;
+import bridge.view.BridgeExpressionMaker;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
@@ -9,7 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
+        OutputView outputView = new OutputView(new BridgeExpressionMaker());
         BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
 
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,17 +1,29 @@
 package bridge.view;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
     private static final String BRIDGE_SIZE_INPUT_INTRO_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    private static final String GAME_RESULT_MESSAGE = "\n최종 게임 결과";
+    private static final String GAME_RESULT_EXPRESSION = "%n%n게임 성공 여부: %s";
+    private static final Map<Boolean, String> RESULT_EXPRESSION_MATCHER = Map.of(
+            true, "성공",
+            false, "실패"
+    );
+    private static final String TRY_COUNT_EXPRESSION = "%n총 시도한 횟수: %d";
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    BridgeExpressionMaker bridgeExpressionMaker;
+
+    public OutputView(BridgeExpressionMaker bridgeExpressionMaker) {
+        this.bridgeExpressionMaker = bridgeExpressionMaker;
+    }
+
+    public void printMap(List<String> bridge, List<String> userBridge) {
+        System.out.println(bridgeExpressionMaker.createBridgeExpression(bridge, userBridge));
     }
 
     /**
