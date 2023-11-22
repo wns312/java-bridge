e259dc851d504a43efeb5a5261250e095b266ada
wns312 <wns312@gmail.com>
2023-11-22 17:19:01 +0900

    refactor(ExceptionType): 불필요한 접근제어자 제거

--- src/main/java/bridge/domain/exception/ExceptionType.java
+++ src/main/java/bridge/domain/exception/ExceptionType.java
@@ -1,9 +1,8 @@
 package bridge.domain.exception;
 
 public interface ExceptionType<T> {
-    static final String ERROR_PREFIX = "[ERROR] ";
+    String ERROR_PREFIX = "[ERROR] ";
 
     String getMessage();
-
     T getException();
 }
