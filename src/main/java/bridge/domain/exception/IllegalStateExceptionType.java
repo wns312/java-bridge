d73648dcc3a0c5c02096a8daad16b6cb694929e4
wns312 <wns312@gmail.com>
2023-11-22 17:19:23 +0900

    style(IllegalStateException): 불필요한 공백 제거

--- src/main/java/bridge/domain/exception/IllegalStateExceptionType.java
+++ src/main/java/bridge/domain/exception/IllegalStateExceptionType.java
@@ -3,7 +3,6 @@ package bridge.domain.exception;
 public enum IllegalStateExceptionType implements ExceptionType<IllegalStateException> {
     INVALID_MOVING("게임이 종료되어 이동할 수 없습니다.");
 
-
     private final String message;
 
     IllegalStateExceptionType(String message) {
