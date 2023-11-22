5297f1632b0739d7e1ed09f675f0374c61fcc3de
wns312 <wns312@gmail.com>
2023-11-22 18:09:56 +0900

    refactor(OutputView): 게임 성공 표현의 개행 개수 변경

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class OutputView {
     private static final String BRIDGE_SIZE_INPUT_INTRO_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
     private static final String GAME_RESULT_MESSAGE = "\n최종 게임 결과";
-    private static final String GAME_RESULT_EXPRESSION = "%n%n게임 성공 여부: %s";
+    private static final String GAME_RESULT_EXPRESSION = "%n게임 성공 여부: %s";
     private static final Map<Boolean, String> RESULT_EXPRESSION_MATCHER = Map.of(
             true, "성공",
             false, "실패"
