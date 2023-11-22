7dcb2ca67c6f85c7b0d51658a2346827d8b1889e
jinsu868 <mjinsu098@gmail.com>
2023-11-22 15:07:14 +0900

    feat(bridge): 다리 길이 검증 로직 추가

---
+++ src/main/java/bridge/error/ErrorCode.java
@@ -0,0 +1,22 @@
+package bridge.error;
+
+public enum ErrorCode {
+    INVALID_BRIDGE_LENGTH("유효하지 다리 길이입니다.");
+
+
+    private static final String MESSAGE_FORMAT = "[ERROR] %s";
+
+    private String message;
+
+    private ErrorCode(String message) {
+        this.message = String.format(MESSAGE_FORMAT, message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public IllegalArgumentException getException() {
+        return new IllegalArgumentException(getMessage());
+    }
+}
