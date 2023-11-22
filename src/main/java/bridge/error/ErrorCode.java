83794bc0d82db468e83a96812efe14c967215e79
jinsu868 <mjinsu098@gmail.com>
2023-11-22 16:43:39 +0900

    feat(view): 이동 경로 입력받는 기능 구현

--- src/main/java/bridge/error/ErrorCode.java
+++ src/main/java/bridge/error/ErrorCode.java
@@ -2,7 +2,8 @@ package bridge.error;
 
 public enum ErrorCode {
     INVALID_BRIDGE_LENGTH("유효하지 다리 길이입니다."),
-    INVALID_BRIDGE_SIZE_TYPE("다리 길이에는 정수값이 들어와야 합니다.");
+    INVALID_BRIDGE_SIZE_TYPE("다리 길이에는 정수값이 들어와야 합니다."),
+    INVALID_MOVING("유효하지 않은 이동 경로입니다.");
 
 
     private static final String MESSAGE_FORMAT = "[ERROR] %s";
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -36,8 +36,18 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String input = Console.readLine();
+        validateMoving(input);
+        return input;
     }
+
+    private void validateMoving(String input) {
+        if (input.equals("U") || input.equals("D")) {
+            return;
+        }
+        throw ErrorCode.INVALID_MOVING.getException();
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
